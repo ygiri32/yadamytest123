96a3382e34cb3c462ed8ba56700152bc3a5f6da0
dyada <ygiri32@gmail.com>
2023-07-25 19:21:35 +0530

    yadanew test

---
+++ C1-version.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = "~>1.3.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.54.0"
+    }
+  }
+
+# provider Block
+provider "aws" {
+
+  region  = "ap-south-1"
+  profile = "default"
+}

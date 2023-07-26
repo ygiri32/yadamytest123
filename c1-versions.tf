6d6ada42f6d5d66d198877af06730b260dd2e69a
dyada <ygiri32@gmail.com>
2023-07-26 19:24:02 +0530

    yadanew test1

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,4 +1,4 @@
-# Terraform Block
+ Terraform Block
 terraform {
   required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
@@ -11,7 +11,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
- 
+  region = "ap-south-1"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  

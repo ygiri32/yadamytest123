0aadce594f50cdffab3fe9ff21155be2983380c0
dyada <ygiri32@gmail.com>
2023-07-26 19:34:01 +0530

    yadanew test1

--- C2-s3.tf
+++ C2-s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "doragandla1" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "dora12345"
     Environment = "Dev"
   }
 }
